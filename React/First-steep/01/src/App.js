f2f29181de2704fe86812083575d7c29d1b82299
alexpodelinskii <nikitik00787@gmail.com>
2020-12-16 00:08:20 +0300

    addasdasdasdsadsa

--- React/First-steep/01/src/App.js
+++ React/First-steep/01/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is my first steep in react3326
+          This is my first steep in react 01
         </p>
         <a
           className="App-link"
